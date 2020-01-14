a67edfacdec5129ead4002e3b3c42ee28534b254
starodubenko <r.starodubenko@tinkoff.ru>
2020-01-14 23:17:45 +0300

    Updates

---
+++ precache-manifest.fd8b64a777f2c7788b93d5d4f0b7963e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3075f088346f286acbde8e91392c60ab",
+    "url": "/coincap/index.html"
+  },
+  {
+    "revision": "b736a2a6dc0282a8da43",
+    "url": "/coincap/static/css/main.81e8e500.chunk.css"
+  },
+  {
+    "revision": "fdd690f5a0486c24e411",
+    "url": "/coincap/static/js/2.6ad2f152.chunk.js"
+  },
+  {
+    "revision": "9297a5aae1239d41135a0cb94b9240f2",
+    "url": "/coincap/static/js/2.6ad2f152.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b736a2a6dc0282a8da43",
+    "url": "/coincap/static/js/main.dd5cba87.chunk.js"
+  },
+  {
+    "revision": "f4111a9fc1a9bd3c9718",
+    "url": "/coincap/static/js/runtime-main.7383296d.js"
+  }
+]);
